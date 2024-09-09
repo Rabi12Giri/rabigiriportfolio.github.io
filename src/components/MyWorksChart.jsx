39bbf2967d4277cdd90869c81e749c92eac0d73a
Rabi Giri <np01cp4a210220@islingtoncollege.edu.np>
2024-09-09 22:05:52 +0545

    Extra section added

--- src/components/MyWorksChart.jsx
+++ src/components/MyWorksChart.jsx
@@ -3,7 +3,7 @@ import { aboutMeData } from "../constants/info";
 
 const MyWorksChart = () => {
   return (
-    <div className="relative z-10 mt-16 lg:mt-0  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-y-12 lg:gap-y-0  pb-12 ">
+    <div className=" mt-16 lg:mt-0  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-y-12 lg:gap-y-0  pb-12 ">
       <div className="flex items-center gap-3">
         <h1 className="text-5xl font-semibold text-primaryRed ">
           {aboutMeData.experience}
